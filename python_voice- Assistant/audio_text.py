cac95abdaa53500b8de0613d4bc2cb11e9d31371
pratik ray <50998329+raypratik9@users.noreply.github.com>
2020-04-14 21:53:59 +0530

    Update audio_text.py

--- python_voice- Assistant/audio_text.py
+++ python_voice- Assistant/audio_text.py
@@ -1,22 +1,27 @@
-  
 import speech_recognition as sr 
 import pyttsx3
 
-sample_rate = 48000
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
-chunk_size = 2048
+def listen():
+    sample_rate = 48000
+    chunk_size = 2048
+    r = sr.Recognizer()
+    with sr.Microphone() as source: 
+        r.adjust_for_ambient_noise(source) 
+        print ("Say Something")
 
-r = sr.Recognizer()
+        audio = r.listen(source) 
+        text = r.recognize_google(audio) 
+        print ("you said: " + text )
+    return text
 
-with sr.Microphone() as source: 
-    r.adjust_for_ambient_noise(source) 
-    print ("Say Something")
-
-    audio = r.listen(source) 
-    text = r.recognize_google(audio) 
-    print ("you said: " + text )
+if __name__=="__main__":
+    text=listen()
+    speak(text)
 
 
-engine = pyttsx3.init()
-engine.say(text)
-engine.runAndWait()
+
